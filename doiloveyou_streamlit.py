a45882ef86dac82e625a01b678adaae6bad30ff0
00nyx00 <gtandreea03@gmail.com>
2025-09-27 14:54:40 +0200

    Add files via upload

---
+++ doiloveyou_streamlit.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.title("Do I love you?")
+
+# Ask for input via Streamlit
+name = st.text_input("Who are you? 🧐").casefold().strip()
+
+love = ["marina", "majdi", "su", "kira", "mama"]
+nope = ["hitler", "pedophile", "carrie bradshaw", "horia brenciu"]
+
+# Only check once input is given
+if name:
+    if name in love:
+        st.success(f"I love you, {name.title()} 🩷")
+    elif name == "nyx":
+        st.info("Always gotta love myself 💋")
+    elif name in nope:
+        st.error("Ew. No. Get out of here. 🤢")
+    else:
+        st.write("You're probably alright I guess. 👍")
